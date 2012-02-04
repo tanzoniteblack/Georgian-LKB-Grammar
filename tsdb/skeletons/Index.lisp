c4270b226b881fda568005580946f34cd1e5872d
Ryan Smith <rnsmith2@gmail.com>
2012-02-03 17:53:19 -0800

    Created corpus item file in tsdb directory.

--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -19,6 +19,7 @@
 ;;; the list lexicographically before output.
 ;;;
 
-(((:path . "lab2") (:content . "Test suite created in lab 2")))
+(((:path . "lab2") (:content . "Test suite created in lab 2"))
+((:path . "corpus") (:content . "Test suite created for corpus.")))
 
  
